3aab526e42039afd7cba091a42943b08d8d660ed
Lawrence Onah <paplow01@gmail.com>
2021-06-28 19:39:03 +0100

    :heavy_plus_sign: Added 1337x list action

--- src/drivers/1337x.js
+++ src/drivers/1337x.js
@@ -7,6 +7,30 @@ module.exports = class One337x extends Crawler {
 	}
 
 	/**
+	 * @param  string url
+	 */
+	getLinks(url) {
+		return this.browse(url, function () {
+			var links = [];
+
+			// Get response data
+			$("table.table-list tbody tr").each(function () {
+				var path = $(this)
+						.children("td.name")
+						.find("a:nth(1)")
+						.attr("href")
+						.trim(),
+					seeds = $(this).children("td.seeds").text().trim(),
+					leeches = $(this).children("td.leeches").text().trim(),
+					size = $(this).children("td.size").contents().get(0).nodeValue.trim();
+				if (path && size) links.push({ size, path, seeds, leeches });
+			});
+
+			return { links: links };
+		});
+	}
+
+	/**
 	 * Search torrent
 	 * @param  string query
 	 * @param  Number page=1
@@ -22,30 +46,8 @@ module.exports = class One337x extends Crawler {
 
 			const data = [];
 
-			const { links } = await this.browse(
-				`${this.endpoint}/search/${query}/${page}/`,
-				function () {
-					var links = [];
-
-					// Get response data
-					$("table.table-list tbody tr").each(function () {
-						var path = $(this)
-								.children("td.name")
-								.find("a:nth(1)")
-								.attr("href")
-								.trim(),
-							seeds = $(this).children("td.seeds").text().trim(),
-							leeches = $(this).children("td.leeches").text().trim(),
-							size = $(this)
-								.children("td.size")
-								.contents()
-								.get(0)
-								.nodeValue.trim();
-						if (path && size) links.push({ size, path, seeds, leeches });
-					});
-
-					return { links: links };
-				}
+			const { links } = await this.getLinks(
+				`${this.endpoint}/search/${query}/${page}/`
 			);
 
 			if (!links) return { data };
@@ -87,7 +89,68 @@ module.exports = class One337x extends Crawler {
 			});
 
 			await Promise.all(promises);
-			await this.cache.set(cacheKey, data);
+			await this.cache.set(cacheKey, { data });
+
+			return { data };
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	}
+
+	/**
+	 * Get list
+	 * @param  Number page=1
+	 */
+	async list(page = 1) {
+		try {
+			// Get from cache first
+			const cacheKey = page + this.endpoint;
+			const cachedResponse = await this.cache.get(cacheKey);
+			if (cachedResponse) return cachedResponse;
+
+			const data = [];
+			const { links } = await this.getLinks(`${this.endpoint}/home/`);
+
+			if (!links) return { data };
+
+			const promises = links.map(async (link) => {
+				const result = await this.browse(
+					this.endpoint + link.path,
+					function () {
+						var name = $("div.box-info-heading h1").text().trim();
+
+						var description = $("div#mCSB_1_container p").text().trim();
+
+						var url = $("ul.dropdown-menu li:nth(0)")
+							.find("a")
+							.attr("href")
+							.trim();
+
+						var magnetic_link = $(
+							"div.torrent-detail-page div:nth-child(1) ul:nth(0)"
+						)
+							.children("li:nth(0)")
+							.find("a:nth(0)")
+							.attr("href")
+							.trim();
+
+						return {
+							name: name,
+							description: description,
+							url: url,
+							magnetic_link: magnetic_link,
+						};
+					}
+				);
+
+				if (result && result.name && result.url) {
+					delete link.path;
+					data.push({ ...result, ...link });
+				}
+			});
+
+			await Promise.all(promises);
+			await this.cache.set(cacheKey, { data });
 
 			return { data };
 		} catch (error) {
