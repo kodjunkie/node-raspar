44e322369355db5943c4921408b0edd0cd5f1656
Lawrence Onah <paplow01@gmail.com>
2021-07-13 02:33:34 +0100

    :zap: Added stealth and adblocker plugin

--- src/lib/crawler.js
+++ src/lib/crawler.js
@@ -1,6 +1,8 @@
-const puppeteer = require("puppeteer");
 const Cache = require("./cache");
-const { cache: cacheConfig } = require("../config");
+const puppeteer = require("puppeteer-extra");
+const cacheConfig = require("../config/cache");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 
 module.exports = class Crawler {
 	constructor(options) {
@@ -8,6 +10,9 @@ module.exports = class Crawler {
 		let cacheOptions;
 		if (options) cacheOptions = options.cache;
 		this.cache = new Cache(cacheConfig, cacheOptions);
+
+		// Browser launch check
+		this.isLaunched = false;
 
 		// Throw error for methods not found
 		return new Proxy(this, {
@@ -22,27 +27,40 @@ module.exports = class Crawler {
 		});
 	}
 
-	/**
-	 * Web crawler
-	 * @param  {} url
-	 * @param  function transform
+	/*
+	 * Initialize the browser
+	 * Ensure only a browser is running
 	 */
-	browse(url, transform) {
-		return new Promise(async (resolve, reject) => {
-			const browser = await puppeteer.launch({
+	async launchBrowser() {
+		if (!this.isLaunched) {
+			puppeteer.use(StealthPlugin());
+			puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+
+			this.browser = await puppeteer.launch({
 				args: ["--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"],
+				headless: true,
 			});
 
+			this.isLaunched = true;
+		}
+	}
+
+	/**
+	 * Web crawler
+	 * @param url
+	 * @param transform
+	 */
+	scrape(url, transform) {
+		return new Promise(async (resolve, reject) => {
 			try {
-				const page = await browser.newPage();
+				await this.launchBrowser();
+				const page = await this.browser.newPage();
 				await page.goto(url, { waitUntil: "load", timeout: 0 });
 				await page.addScriptTag({ path: require.resolve("jquery") });
 				let response = await page.evaluate(transform);
-				browser.close();
 				return resolve(response);
-			} catch (e) {
-				browser.close();
-				return reject(e);
+			} catch (error) {
+				return reject(error);
 			}
 		});
 	}
