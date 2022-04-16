93fb869bae8f4dd6102096e36a02eadc15e0e924
Lawrence Onah <paplow01@gmail.com>
2022-04-16 12:44:24 +0100

    :lock: Refactoring and enhancement

--- src/config/cache.js
+++ src/config/cache.js
@@ -2,7 +2,7 @@ module.exports = {
 	store: require("cache-manager-fs-hash"),
 	options: {
 		path: "./temp",
-		ttl: 60 * 60 * 24,
+		ttl: 60 * 60 * 24 * 14, // 2 weeks
 		maxsize: 1024,
 		subdirs: false,
 		zip: true,
